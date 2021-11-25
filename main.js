779290433f1103579a332a342fc6932f6842af0c
Yara Tavares <yaracriistavares@gmail.com>
2021-11-25 19:54:03 -0300

    responsive

--- main.js
+++ main.js
@@ -41,7 +41,13 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true 
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWripperSize: true
+        }
+    }
   });
 
 // Scroll reveal: mostrar elementos quando der scroll na página
